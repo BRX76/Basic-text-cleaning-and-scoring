e04bc93ed065043a0521a3001ee5be32d7027bc3
dekel r <dekel.reu@gmail.com>
2020-11-26 17:16:08 +0200

    moving the amazon number of items to config

--- bl/search_amazon_items_in_alibaba.py
+++ bl/search_amazon_items_in_alibaba.py
@@ -1,6 +1,6 @@
 import logging
 
-from config import LOGGER_NAME
+from config import LOGGER_NAME, NUMBER_OF_ITEMS
 from bl.get_data import alibaba_get_search_result_titles, parse_alibaba_search_result
 
 log = logging.getLogger(LOGGER_NAME)
@@ -10,7 +10,9 @@ def handle_alibaba_search(list_without_empty_amazon):
     log.info('Getting results from alibaba, this may take some time.')
     alibaba_results = []
     results_counter = 1
-    for item in list_without_empty_amazon[:100]:
+
+    number_of_items = min(len(list_without_empty_amazon), NUMBER_OF_ITEMS)
+    for item in list_without_empty_amazon[:number_of_items]:
         try:
             # Later - Use multi threading for the io operation
             search_result = alibaba_get_search_result_titles(search_string=item)
